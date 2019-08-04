7d4a817a27c304feebfdfca091a8accc5ca2cb37
blackwhitetony <blackwhitetony@outlook.com>
2019-08-04 13:14:31 +0800

    文章显示卡片添加标签，去除评论

--- post-list.php
+++ post-list.php
@@ -2,24 +2,13 @@
 
     <div class="mdui-card-primary">
         <a class="mdui-card-primary-title title-fixed" href="<?php $this->permalink() ?>"><?php $this->title() ?></a>
-        <a class="mdui-card-primary-subtitle title-fixed"><?php $this->tags(', ', true, 'none')?></a>
     </div>
-
-    <div class="mdui-card-content nokatex">
-        <?php
-            if (isset($this->fields->summary)) { echo $this->fields->summary; }
-            else $this->excerpt(200, '...');
-        ?>
-    </div>
-
+    <div class="mdui-card-content nokatex"><?php if (isset($this->fields->summary)) { echo $this->fields->summary; } else $this->excerpt(200, '...'); ?></div>
     <div class="mdui-card-actions">
         <span class="more">
             <i class="mdui-icon material-icons index-icon">&#xe192;</i> <time datetime="<?php $this->date('c'); ?>" itemprop="datePublished"><?php $this->date('Y-m-d'); ?></time> &nbsp;&nbsp;
             <i class="mdui-icon material-icons index-icon">&#xe866;</i> <?php $this->category('  '); ?> &nbsp;&nbsp;
-            <a href="<?php $this->permalink() ?>#comments"><i class="mdui-icon material-icons index-icon">&#xe0b9;</i> <?php $this->commentsNum('%d 条评论'); ?></a>
+            <?php $this->tags(' | ', true, 'none')?>
         </span>
-        <div class="mdui-float-right">
-            <?php if ($this->category=='sol'):?><?php $this->need('luogu.php')?><?php endif;?></div>
     </div>
-
 </div>
