454202dd9da2ebd1a9297bcf4149002b8b2ffa32
blackwhitetony <blackwhitetony@outlook.com>
2019-08-05 10:14:03 +0800

    添加访问记录

--- post-list.php
+++ post-list.php
@@ -8,7 +8,8 @@
         <span class="more">
             <i class="mdui-icon material-icons index-icon">&#xe192;</i> <time datetime="<?php $this->date('c'); ?>" itemprop="datePublished"><?php $this->date('Y-m-d'); ?></time> &nbsp;&nbsp;
             <i class="mdui-icon material-icons index-icon">&#xe866;</i> <?php $this->category(' | '); ?> &nbsp;&nbsp;
-            <i class="mdui-icon material-icons index-icon">&#xe867;</i> <?php $this->tags(' | ', true, 'none')?>
+            <i class="mdui-icon material-icons index-icon">&#xe867;</i> <?php $this->tags(' | ', true, 'none')?> &nbsp; &nbsp;
+            <a class="mdui-float-right"><i class="mdui-icon material-icons index-icon">&#xe417;</i> <?php Views_Plugin::theViews(); ?></a>
         </span>
     </div>
 </div>
